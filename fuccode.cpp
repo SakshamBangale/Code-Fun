0626a8822b1425d6986d28a184a5b27506f2949d
Saksham Bangale <sakshambangale779@gmail.com>
2026-02-12 23:43:40 +0530

    Fix calculation and output for sum of two numbers "For testing"

--- fuccode.cpp
+++ fuccode.cpp
@@ -5,8 +5,15 @@ using namespace std;
 
 int main(){
 
-   int num1,num2;
+   int num1, num2,sum;
 
-   cout<<"Sum : "<<num1+num2;
+   cout<<"Enter a Value for Calculting value ";
+   cout<<"Enter a 1st Value : ";
+   cin>>num1;
+   cout<<"Enter a 2nd Value : ";
+   cin>>num2;
+
+   sum = num1+num2;
+   cout<<"Sum : "<<sum;
    
 }
