5c6a4bf06669e77e8bf6336ab01781182552ebf2
Saksham Bangale <sakshambangale779@gmail.com>
2026-02-13 23:23:36 +0530

    Update fuccode.cpp

--- fuccode.cpp
+++ fuccode.cpp
@@ -7,11 +7,11 @@ int main()
 
   int a;
 
-  for (int i = 1; i < 5; i++)
+  for (int i = 1; i < 10; i++)
   {
     for (int j = 1; j < i; j++)
     {
-      cout << j ;
+      cout << j+i ;
     }
     cout << endl;
   }
