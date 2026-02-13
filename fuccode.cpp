ab42df4654207b21b657eaaf71e85b3fe2fdf353
Saksham Bangale <sakshambangale779@gmail.com>
2026-02-13 23:25:32 +0530

    Update fuccode.cpp

--- fuccode.cpp
+++ fuccode.cpp
@@ -16,3 +16,21 @@ int main()
     cout << endl;
   }
 }
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  int a;
+
+  for (int i = 1; i < 5; i++)
+  {
+    for (int j = 1; j < i; j+i)
+    {
+      cout << "*" ;
+    }
+    cout << endl;
+  }
+}
