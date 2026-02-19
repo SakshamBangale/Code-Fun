f2c4d8f5ee7f1c2199a9c977acfb4475eb149e9b
SakshamBangale <sakshambangale779@gmail.com>
2026-02-19 23:07:34 +0530

    New practice

--- test6.cpp
+++ test6.cpp
@@ -1,54 +1,121 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-class Solution
-{
+// class Solution
+// {
 
-    int factorial = 1;
-    int i{1};
+//     int factorial = 1;
+//     int i{1};
 
-public:
-    // FOR LOOP
+// public:
+//     // FOR LOOP
 
-    int forfact(int x)
-    {
+//     int forfact(int x)
+//     {
 
-        for (; i <= x; i++)
-        {
-            /* code */
-            factorial = factorial * i;
-        }
-        return factorial;
-    }
+//         for (; i <= x; i++)
+//         {
+//             /* code */
+//             factorial = factorial * i;
+//         }
+//         return factorial;
+//     }
 
-    int whilefact(int y)
-    {
-        while (i <= y)
-        {
-            factorial = factorial * i;
-            i--;
-        }
-        return factorial;
-    }
-    int dowhilefact(int z){
+//     int whilefact(int y)
+//     {
+//         while (i <= y)
+//         {
+//             factorial = factorial * i;
+//             i--;
+//         }
+//         return factorial;
+//     }
+//     int dowhilefact(int z){
 
-        do
-        {
-            factorial = factorial*i;
-            i--;
-            /* code */
-        } while (i<=z);
-        return factorial;
-    }
-};
+//         do
+//         {
+//             factorial = factorial*i;
+//             i--;
+//             /* code */
+//         } while (i<=z);
+//         return factorial;
+//     }
+// };
 
-int main()
-{
+// int main()
+// {
 
-Solution obj;
+// Solution obj;
 
 
-cout<<obj.forfact(5);
-    return 0;
-}
+// cout<<obj.forfact(5);
+//     return 0;
+// }
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// class Solution
+// {
+// public:
+
+//     // FOR LOOP FACTORIAL
+//     int factorialFor(int n)
+//     {
+//         int fact = 1;
+
+//         for (int i = 1; i <= n; i++)
+//         {
+//             fact *= i;
+//         }
+
+//         return fact;
+//     }
+
+//     // WHILE LOOP FACTORIAL
+//     int factorialWhile(int n)
+//     {
+//         int fact = 1;
+//         int i = 1;
+
+//         while (i <= n)
+//         {
+//             fact *= i;
+//             i++;
+//         }
+
+//         return fact;
+//     }
+
+//     // DO-WHILE LOOP FACTORIAL
+//     int factorialDoWhile(int n)
+//     {
+//         int fact = 1;
+//         int i = 1;
+
+//         if (n == 0) return 1; // handle 0!
+
+//         do
+//         {
+//             fact *= i;
+//             i++;
+//         } while (i <= n);
+
+//         return fact;
+//     }
+// };
+
+// int main()
+// {
+//     Solution obj;
+
+//     cout << "For Loop: " << obj.factorialFor(5) << endl;
+//     cout << "While Loop: " << obj.factorialWhile(5) << endl;
+//     cout << "Do While Loop: " << obj.factorialDoWhile(5) << endl;
+
+//     return 0;
+// }
