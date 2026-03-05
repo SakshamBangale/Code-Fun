59bbd2b66041d1e78e495ae754fa05f00d273529
Saksham Bangale <sakshambangale779@gmail.com>
2026-03-05 22:36:32 +0530

    Refactor output in nested loops for pattern generation
    Updated output formatting in nested loops for better clarity.

--- test11.cpp
+++ test11.cpp
@@ -7,7 +7,10 @@ public:
 for(int i = 1;i<n;i++){
 
     for(int j=1;j<=i;j++){
-        cout<<"*"
+        cout<<" ";
+    }
+    for(int j=1;j<i*2-1;j++){
+        cout<<"*";
     }
     cout<<endl;
 }
@@ -16,7 +19,7 @@ for(int i = 1;i<n;i++){
 for(int i = 1;i<n;i++){
 
     for(int j=1;j<=i;j++){
-        cout<<"*"
+        cout<<"*";
     }
     cout<<endl;
 }
