43d3e1008409f4a11ee13b42615bc98de4d22575
Saksham Bangale <sakshambangale779@gmail.com>
2026-04-04 23:09:09 +0530

    Update test5.cpp
    Removed the do-while and while loops for factorial calculation.

--- test5.cpp
+++ test5.cpp
@@ -2,28 +2,7 @@
 using namespace std;
 
 int main()
-{
-    // -------- DO WHILE --------
-    int i1{5}, factorial1{1};
 
-    do
-    {
-        factorial1 *= i1;
-        i1--;
-    } while (i1 >= 1);
-
-    cout << "Do While Factorial: " << factorial1 << endl;
-
-
-    // -------- WHILE --------
-    int i2 = 5;
-    int factorial2 = 1;
-
-    while (i2 >= 1)
-    {
-        factorial2 *= i2;
-        i2--;
-    }
 
     cout << "While Factorial: " << factorial2 << endl;
 
