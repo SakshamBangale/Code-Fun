874feb265f3938bca8288d2cde96e8deafc0ad36
Saksham Bangale <sakshambangale779@gmail.com>
2026-04-09 23:16:59 +0530

    Add maxSubarraySum function to calculate max sum
    Implement function to find maximum sum of contiguous subarray

---
+++ dsq12.c
@@ -0,0 +1,34 @@
+// Given an integer array, find the maximum sum of a contiguous subarray.
+
+#include <stdio.h>
+#include <limits.h>
+
+int maxSubarraySum(int arr[], int n) {
+    int max_sum = INT_MIN;
+    int current_sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        current_sum += arr[i];
+
+        if (current_sum > max_sum) {
+            max_sum = current_sum;
+        }
+
+        if (current_sum < 0) {
+            current_sum = 0;
+        }
+    }
+
+    return max_sum;
+}
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = maxSubarraySum(arr, n);
+
+    printf("Maximum subarray sum is: %d\n", result);
+
+    return 0;
+}
