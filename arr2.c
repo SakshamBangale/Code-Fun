b0a371fc05b7451f2e9a420abe4061436188ba41
Saksham Bangale <sakshambangale779@gmail.com>
2026-04-14 23:38:33 +0530

    Add program to read an array from a file

---
+++ arr2.c
@@ -0,0 +1,27 @@
+// Read Array from File
+#include <stdio.h>
+
+int main() {
+    int arr[5], i;
+    FILE *fp;
+
+    fp = fopen("data.txt", "r");
+
+    if (fp == NULL) {
+        printf("File not found");
+        return 1;
+    }
+
+    for (i = 0; i < 5; i++) {
+        fscanf(fp, "%d", &arr[i]);
+    }
+
+    fclose(fp);
+
+    printf("Array read from file:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
