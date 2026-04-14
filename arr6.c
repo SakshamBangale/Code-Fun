11ab9003ea8cd03291245de96024db2af870a1f0
Saksham Bangale <sakshambangale779@gmail.com>
2026-04-14 23:39:54 +0530

    Add program to search for an element in a file

---
+++ arr6.c
@@ -0,0 +1,30 @@
+// Search Element
+#include <stdio.h>
+
+int main() {
+    int num, key = 30, found = 0;
+    FILE *fp;
+
+    fp = fopen("data.txt", "r");
+
+    if (fp == NULL) {
+        printf("File not found");
+        return 1;
+    }
+
+    while (fscanf(fp, "%d", &num) != EOF) {
+        if (num == key) {
+            found = 1;
+            break;
+        }
+    }
+
+    fclose(fp);
+
+    if (found)
+        printf("Element found");
+    else
+        printf("Element not found");
+
+    return 0;
+}
