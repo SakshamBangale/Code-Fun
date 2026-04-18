09322e479b2c378f237095c3f88ce2fa84908e87
Saksham Bangale <sakshambangale779@gmail.com>
2026-04-18 22:21:56 +0530

    Remove main function from arr4.c

--- arr4.c
+++ arr4.c
@@ -4,24 +4,4 @@
 
 #include <stdio.h>
 
-int main() {
-    int num, sum = 0;
-    FILE *fp;
-
-    fp = fopen("data.txt", "r");
-
-    if (fp == NULL) {
-        printf("File not found");
-        return 1;
-    }
-
-    while (fscanf(fp, "%d", &num) != EOF) {
-        sum += num;
-    }
-
-    fclose(fp);
-
-    printf("Sum of elements = %d", sum);
-
-    return 0;
-}
+int ma
